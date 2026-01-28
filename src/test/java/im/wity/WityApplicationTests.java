7363028a3512d652568328d5c64ed42fb3ab92c1
심지훈 <wlgns2223@gmail.com>
2026-01-28 21:35:58 +0900

    initial commit

---
+++ src/test/java/im/wity/WityApplicationTests.java
@@ -0,0 +1,13 @@
+package im.wity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
