35e3c1f9371234ade3c3010c6e3ffb8d23223f37
심지훈 <wlgns2223@gmail.com>
2026-01-30 08:50:14 +0900

    feat: password vo

--- src/main/java/im/wity/entity/User.java
+++ src/main/java/im/wity/entity/User.java
@@ -1,8 +1,11 @@
 package im.wity.entity;
 
 import im.wity.constant.AuthProvider;
+import im.wity.entity.vo.Password;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -10,26 +13,27 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "user")
-@NoArgsConstructor
+@NoArgsConstructor(access =AccessLevel.PROTECTED)
 public class User {
     @Id
     private Long id;
 
     @Column(nullable = false)
-    String email;
+    private String email;
 
-    @Column
-    String password;
+    @Embedded
+    private Password password;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    AuthProvider authProvider;
+    private AuthProvider authProvider;
 
     @Column(nullable = false,name = "is_Oauth")
-     Boolean isOauth;
+    private Boolean isOauth;
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
@@ -40,7 +44,7 @@ public class User {
     private LocalDateTime updatedAt;
 
     @Builder
-    public User(String email,String password, AuthProvider authProvider){
+    public User(String email,Password password, AuthProvider authProvider){
         this.email = email;
         this.password = password;
         this.authProvider = authProvider;
@@ -48,11 +52,6 @@ public class User {
     }
 
     public static User createLocalUser(String email, String password, AuthProvider authProvider){
-        return User.builder().email(email).password(password).authProvider(authProvider).build();
+        return User.builder().email(email).password(Password.Of(password)).authProvider(authProvider).build();
     }
-
-
-
-
-
 }
--- src/main/java/im/wity/entity/vo/Password.java
+++ src/main/java/im/wity/entity/vo/Password.java
@@ -1,4 +1,40 @@
 package im.wity.entity.vo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Password {
+
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    
+    @Column(name = "password")
+    String encryptedPassword;
+
+    private Password(String password){
+        this.encryptedPassword = password;
+    }
+
+    public static Password Of(String plain){
+        validate(plain);
+        return new Password(encode(plain));
+    }
+
+    private static void validate(String plainPassword){
+        if(plainPassword == null || plainPassword.isEmpty()){
+            throw new IllegalArgumentException("비밀번호가 비어있습니다. ");
+        }
+
+        // validation logic...
+    }
+
+    private static String encode(String plainPassword){
+
+        return passwordEncoder.encode(plainPassword);
+    }
 }
